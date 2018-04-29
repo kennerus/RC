e2a9f1e0465576cf18a0e7b9d923aff8fd2d3ca5
dmitryi <apuc06@mail.ru>
2018-04-29 13:16:33 +0300

    final

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -19,6 +19,19 @@ class Detail extends React.Component {
   componentDidMount() {
     const currencyId = this.props.match.params.id;
 
+    this.fetchCurrency(currencyId);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.location.pathname !== nextProps.location.pathname) {
+      // Get new currency id from url
+      const newCurrencyId = nextProps.match.params.id;
+
+      this.fetchCurrency(newCurrencyId);
+    }
+  }
+
+  fetchCurrency(currencyId) {
     this.setState({ loading: true });
 
     fetch(`${API_URL}/cryptocurrencies/${currencyId}`)
@@ -37,6 +50,8 @@ class Detail extends React.Component {
         });
       })
   }
+
+
 
   render() {
     const { loading, error, currency } = this.state;
